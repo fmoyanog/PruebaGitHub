48a6ad9d82af5a1b091608e731f310b8ebf333a8
fmoyanog <40763740+fmoyanog@users.noreply.github.com>
2025-05-29 13:05:19 +0200

    Segundo cambio VAriables

--- 41PrjGitHubPrueba/src/EjemplGit.java
+++ 41PrjGitHubPrueba/src/EjemplGit.java
@@ -4,6 +4,8 @@ public class EjemplGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Comiezo de Proyecto GIT");
+		String nombre="paco";
+		
 	}
 
 }
